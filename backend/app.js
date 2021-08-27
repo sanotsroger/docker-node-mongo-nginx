ee97a28139e925483481d635b54876eb97c21619
Roger Santos <rogerioantoniodossantos@outlook.com>
2021-08-26 21:43:13 -0300

    Backend implemented

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,29 @@ const express = require('express')
 const restful = require('node-restful')
 const server = express()
 const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // Database
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
-// Test
-server.get('/', (req, res, next) => {
-    return res.send('Backend ...')
-});
+// Middlewares
+server.use(bodyParser.urlencoded({extended:true}));
+server.use(bodyParser.json())
+server.use(cors())
+
+// Mapping Object
+const Client = restful.model('Client', {
+    name: { type: String, required: true}
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true});
+
+// Router
+Client.register(server, '/clients')
 
 // Start Server
 server.listen(3000)
